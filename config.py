8533cec07c00b3b1ddc52267e69a38728009dfbf
Aida Real <areal060781@gmail.com>
2020-06-27 19:54:45 -0500

    Follow, unfollow and list followers and following

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASKY_POSTS_PER_PAGE = 10
+    FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
